bf0462a2c0284f607b0c727b4c33ddc1477529dc
logopedistadeborabibiano-cmd <logopedistadeborabibiano@gmail.com>
2026-08-07 15:20:36 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,23 +2,20 @@
 ======================================================
 CLARA AROUND THE WORLD
 Data
-Versione 4.1
+Versione 4.2 - Slug corretti
 ======================================================
 */
-
 const initiatives = [
-
     {
         city: "Milano",
         country: "Italia",
         lat: 45.4642,
         lng: 9.1900,
         active: false,
-
         initiatives: [
             {
                 title: "La colazione di Clara",
-                slug: "la-colazione-di-clara"
+                slug: "la-colazione-di-clara-la-golosastra"
             },
             {
                 title: "Run with Clara for Pediatria LILT",
@@ -26,58 +23,51 @@ const initiatives = [
             },
             {
                 title: "Un cuore per Clara",
-                slug: "un-cuore-per-clara"
+                slug: "un-cuore-per-clara-larte-e-la-musica-per-ricordare-clara"
             }
         ]
     },
-
     {
         city: "Aprica",
         country: "Italia",
         lat: 46.1528,
         lng: 10.1519,
         active: false,
-
         initiatives: [
             {
                 title: "Camminate con Clara nel Cuore",
-                slug: "camminate-con-clara-nel-cuore"
+                slug: "aprica-laghi-torena-domenica-5-ottobre-2025"
             },
             {
                 title: "Wild Park",
-                slug: "wild-park"
+                slug: "wild-park-di-aprica-e-caspoggio"
             }
         ]
     },
-
     {
         city: "São Paulo",
         country: "Brasile",
         lat: -23.5505,
         lng: -46.6333,
         active: false,
-
         initiatives: [
             {
                 title: "Ospedale Santa Marcelina",
-                slug: "ospedale-santa-marcelina"
+                slug: "hospital-santa-marcelina-primeira-iniciativa"
             }
         ]
     },
-
     {
         city: "Melbourne",
         country: "Australia",
         lat: -37.8136,
         lng: 144.9631,
         active: false,
-
         initiatives: [
             {
                 title: "Walk 4 Brain Tumour",
-                slug: "walk-4-brain-tumour"
+                slug: "walk-4-brain-cancer"
             }
         ]
     }
-
 ];
